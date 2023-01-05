e52c4d90440221c753454c85dd48135d4f302ef5
skinnyfads <skinnyfads@proton.me>
2023-01-06 06:37:50 +0700

    feat(api): create base routes

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import express from "express";
 import "dotenv/config";
 import "./config/database.js";
+import routes from "./routes/index.js";
 
 const app = express();
 const port = process.env.PORT || 8080;
+
+app.use(express.json());
+app.use(routes);
 
 app.listen(port, () => console.log(`App listening on ${port}`));
