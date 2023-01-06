21d715b829c0078c4fe50083ab99a1295eab3e82
skinnyfads <skinnyfads@proton.me>
2023-01-06 18:43:41 +0700

    feat(api): auto generate time when creating note

--- src/controllers/NoteController.ts
+++ src/controllers/NoteController.ts
@@ -4,9 +4,10 @@ import NoteConstruct from "../models/Note.js";
 
 async function createNote(req: Request, res: Response) {
   const id = await generateId(NoteConstruct);
-  const { title, content, createdAt, author } = req.body;
+  const { title, content, author } = req.body;
 
-  if (title && content && createdAt && author) {
+  if (title && content && author) {
+    const createdAt = +new Date();
     const newNote = new NoteConstruct({ id, title, content, createdAt, author });
     return res.send(await newNote.save());
   }
