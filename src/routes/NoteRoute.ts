ed87a8d24d21961f3ecdf7a7a2e2248cbfe21df1
skinnyfads <skinnyfads@proton.me>
2023-01-07 07:26:30 +0700

    feat(api): remove note

--- src/routes/NoteRoute.ts
+++ src/routes/NoteRoute.ts
@@ -4,5 +4,6 @@ import NoteController from "../controllers/NoteController.js";
 const router = Router();
 
 router.post("/create", NoteController.create);
+router.delete("/remove/:id", NoteController.remove);
 
 export default router;
